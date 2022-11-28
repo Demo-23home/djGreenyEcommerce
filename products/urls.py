2f6914ce5a65d3af67319bdc031dcb9fa4a4ded3
Demo-23home <zezomedoslama@gmail.com>
2022-11-28 19:55:33 +0200

    Brand-list&detail added

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include,path
 
-from .views import ProductDetail,ProductList
+from .views import ProductDetail,ProductList,BrandList,BrandDetail
 
 app_name="products"
 
 urlpatterns = [
     
-    path('',ProductList.as_view()),
-    path('<int:pk>',ProductDetail.as_view())
+    path('',ProductList.as_view(),name='product_list'),
+    path('brands/',BrandList.as_view(),name='brand_list'),
+    path('<int:pk>',ProductDetail.as_view(),name='product_detail'),
+    path('brands/<int:pk>',BrandDetail.as_view(),name='brand_detail'),
 ]
 
 
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product,Brand,Category
 from django.views.generic import ListView,DetailView
 # Create your views here.
 
@@ -11,3 +11,15 @@ class ProductList(ListView):
 
 class ProductDetail(DetailView):
     model=Product
+
+
+class BrandList(ListView):
+    model=Brand
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+    
+
+class BrandDetail(DetailView):
+    model=Brand
